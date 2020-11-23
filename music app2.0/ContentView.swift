15b321cb5423e830de0126d152787b917579c848
Jim <chenjim821ddt@gmail.com>
2020-11-23 18:53:29 +0800

    Initial Commit

---
+++ music app2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  music app2.0
+//
+//  Created by 陳元浚 on 2020/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ music app2.0/music_app2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  music_app2_0App.swift
+//  music app2.0
+//
+//  Created by 陳元浚 on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct music_app2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
